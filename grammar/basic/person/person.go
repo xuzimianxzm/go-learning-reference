a6d0c99ab8c1ee26a9ac94c227d7bd7557fdd7ad
Zimian Xu <zimian.xu@thoughtworks.com>
2021-06-16 16:16:55 +0800

    update the comments

--- grammar/basic/person/person.go
+++ grammar/basic/person/person.go
@@ -24,12 +24,16 @@ func (e Employee) ToString() string {
 	return strconv.Itoa(e.ManagerID) + " " + strconv.Itoa(e.ID) + " " + e.FirstName + " " + e.LastName + " " + e.Address + " " + strconv.Itoa(e.age)
 }
 
+// Notice: any time we pass a value to a function, either as a receiver or as an argument, that data is copied in memory.
 func (e Employee) UpdateFirstName(firstName string) {
 	e.FirstName = firstName
 }
 
-// UpdateFirstNameWithPoint /**
-// any time we pass a value to a function, either as a receiver or as an argument, that data is copied in memory.
+/*
+There are two reasons to use a pointer receiver.
+1. The first is so that the method can modify the value that its receiver points to.
+2. The second is to avoid copying the value on each method call. This can be more efficient if the receiver is a large struct, for example.
+*/
 func (e *Employee) UpdateFirstNameWithPoint(firstName string) {
 	(*e).FirstName = firstName
 }
