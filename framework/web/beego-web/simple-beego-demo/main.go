bada7dc6f0f137e37f155e9bcab3c8bc9e564d36
516654313@qq.com <516654313@qq.com>
2021-07-20 22:26:24 +0800

    add app.conf into simple-beego-demo

--- framework/web/beego-web/simple-beego-demo/main.go
+++ framework/web/beego-web/simple-beego-demo/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/beego/beego/v2/server/web"
+import (
+	"fmt"
+	"github.com/beego/beego/v2/server/web"
+)
 
 func main() {
+	name, _ := web.AppConfig.String("name")
+	user, _ := web.AppConfig.String("mysqluser")
+	password, _ := web.AppConfig.String("mysqlpassword")
+	fmt.Println(name, user, password)
+
 	web.Run()
 }
